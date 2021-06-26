c4f2a26e99c90b61d70d564d2566bd47b3764855
yp-afr <prohorenko.yaroslav@icloud.com>
2021-06-27 01:39:46 +0300

    Добавление мелких фич

--- handlers/users/DelReviewCall.py
+++ handlers/users/DelReviewCall.py
@@ -8,10 +8,10 @@ from utils.dp_api.database import database
 @dp.callback_query_handler(del_review.filter())
 async def delete_post(call: types.CallbackQuery, callback_data: dict):
     review_id = int(callback_data.get("review_id"))
-    try:
-        await database.del_review(review_id)
-        chat_id = types.User.get_current().id
-        msg_id = call.message.message_id
-        await bot.delete_message(chat_id, msg_id)
-    except Exception:
-        await call.message.answer("Упс, что-то пошло не так!")
+    # try:
+    await database.del_review(review_id)
+    chat_id = types.User.get_current().id
+    msg_id = call.message.message_id
+    await bot.delete_message(chat_id, msg_id)
+# except Exception:
+#   await call.message.answer("Упс, что-то пошло не так!")
