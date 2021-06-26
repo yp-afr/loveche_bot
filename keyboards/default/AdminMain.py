6fc1a9857039a1964e5982fbf6134bc5bccd7c43
yp-afr <prohorenko.yaroslav@icloud.com>
2021-06-27 00:57:39 +0300

    Изменение расположения кнопок

--- keyboards/default/AdminMain.py
+++ keyboards/default/AdminMain.py
@@ -4,5 +4,5 @@ from data import names
 markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 markup.add(names.button_create_text, names.button_myposts_text)
 markup.row(names.button_allposts_text)
-markup.row(names.button_admin_text)
-markup.add(names.button_show_reviews)
+markup.row(names.button_admin_text, names.button_show_reviews)
+
