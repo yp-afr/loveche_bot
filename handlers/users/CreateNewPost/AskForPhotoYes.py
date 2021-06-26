f32f418041cf5aba0cbb4b4b1eb9ae811a78e70d
yp-afr <prohorenko.yaroslav@icloud.com>
2021-06-27 02:07:26 +0300

    Мелкие правки

--- handlers/users/CreateNewPost/AskForPhotoYes.py
+++ handlers/users/CreateNewPost/AskForPhotoYes.py
@@ -1,12 +1,16 @@
 from aiogram import types
+from aiogram.dispatcher import FSMContext
 
 from data import names
+from handlers.users.misc import Item
 from loader import dp
 from states.NewItem import NewItem
 
 
 @dp.callback_query_handler(state=NewItem.AskForPhoto, text_contains="yes")
-async def send_photo(call: types.CallbackQuery):
+async def send_photo(call: types.CallbackQuery, state: FSMContext):
     await call.message.delete()
+    data = await state.get_data()
+    item: Item = data.get("item")
     await call.message.answer(names.send_photo_text)
     await NewItem.WithPhoto.set()
