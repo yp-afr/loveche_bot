392aedb7972b9910090fc2b23ba3cb0159c7c00a
yp-afr <prohorenko.yaroslav@icloud.com>
2021-06-28 12:52:33 +0300

    Мелкие исправления

--- data/admins.py
+++ data/admins.py
@@ -1,8 +1,3 @@
 from utils.dp_api.database import database
 
-
-async def get_admins_list():
-    list_admins = await database.get_admins()
-    return list_admins
-
-admins = get_admins_list()
+admins = await database.get_admins()
