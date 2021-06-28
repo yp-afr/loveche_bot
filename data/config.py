aadf9d620b793e0c26f8c13fe3339f279d702354
yp-afr <prohorenko.yaroslav@icloud.com>
2021-06-28 12:40:19 +0300

    Мелкие исправления

--- data/config.py
+++ data/config.py
@@ -7,7 +7,7 @@ from utils.dp_api.database import database
 load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
-admins = database.get_admins()
+admins = []
 host = os.getenv("PGHOST")
 PG_USER = os.getenv("PG_USER")
 PG_PASS = os.getenv("PG_PASS")
