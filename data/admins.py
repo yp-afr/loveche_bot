7743495d81c323d5668031b810088bccc7d4c57b
yp-afr <prohorenko.yaroslav@icloud.com>
2021-06-28 12:55:12 +0300

    Мелкие исправления

--- data/admins.py
+++ data/admins.py
@@ -1,3 +1,12 @@
+import asyncio
+
 from utils.dp_api.database import database
 
-admins = await database.get_admins()
+
+async def get_admins_list():
+    admins = await database.get_admins()
+    return admins
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(get_admins_list())
